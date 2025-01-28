4b6e57fd3a61c087e71f0c3f4ace8ac3b82da104
tommy <tommasominuto@gmail.com>
2025-01-28 22:28:23 +0100

    removed .venv from git tracking

--- .venv/lib/python3.12/site-packages/nordigen/__init__.py
+++
@@ -1,1 +0,0 @@
-from .nordigen import NordigenClient
--- .venv/lib/python3.12/site-packages/nordigen/api/__init__.py
+++
@@ -1,4 +0,0 @@
-from .accounts import AccountApi
-from .agreements import AgreementsApi
-from .institutions import InstitutionsApi
-from .requisitions import RequisitionsApi
