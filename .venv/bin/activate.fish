122f3b04df4391fe017be547772089d6a4d8170b
tommy <tommasominuto@gmail.com>
2025-01-28 22:27:04 +0100

    commit

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tommy/Progetti Python/personal-finance/.venv"
+set -gx VIRTUAL_ENV "/Users/tommy/Progetti Python/transaction-downloader/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
