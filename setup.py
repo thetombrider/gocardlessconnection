7fb13900f544da73fee03ea2adbc5a141a0c9e2f
tommy <tommasominuto@gmail.com>
2025-01-31 14:46:41 +0100

    fixes

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Read README.md with proper encoding
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="fintools",
-    version="0.1",
+    name="gocardless-fintools",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         "python-dotenv",
@@ -12,13 +17,13 @@ setup(
     ],
     entry_points={
         "console_scripts": [
-            "fintools=gocardless_connector.connector:cli",
+            "gocardless-fintools=gocardless_connector.connector:cli",
         ],
     },
     author="thetombrider",
     author_email="tommasominuto@gmail.com",
     description="A CLI tool for managing bank account connections via GoCardless Bank Account Data API",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/thetombrider/gocardlessconnection",
     classifiers=[
